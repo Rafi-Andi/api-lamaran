8d927a4b1ac699e989dc3cf5bf373f590ba4cf4b
Rafi-Andi <rafiandiprayitno9528@gmail.com>
2025-09-30 10:12:23 +0700

    commit file

---
+++ app/Http/Controllers/LowonganKerjaController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\LowonganKerja;
+use Illuminate\Http\Request;
+
+class LowonganKerjaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(LowonganKerja $lowonganKerja)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(LowonganKerja $lowonganKerja)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, LowonganKerja $lowonganKerja)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(LowonganKerja $lowonganKerja)
+    {
+        //
+    }
+}
---
+++ database/migrations/2025_09_30_025943_create_lamarans_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('lamarans', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('lowongan_kerja_id')->constrained();
+            $table->text('surat_lamaran');
+            $table->enum('status', ['menunggu', 'diterima', 'ditolak']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('lamarans');
+    }
+};
